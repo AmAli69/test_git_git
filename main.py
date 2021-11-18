a56d598516895969520c99ba6c1b79ebfa286b6f
yandex <AmAli69@yandex.ru>
2021-11-18 18:12:21 +0300

    add commint hash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print(f'i`m can do commit')
-
+    # f151cae0012617c483ce4a6bb77c0c7fc6f86297
 
 if __name__ == '__main__':
     print_hi('PyCharm')
